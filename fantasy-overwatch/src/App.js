e1e965324623a8fff966fb27b960e7061d5d5cb3
Gary Grumbley <ggrumbley@trackforce.com>
2018-09-25 12:20:01 -0700

    Setup fantasy overwatch redux store

---
+++ fantasy-overwatch/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import SampleComponent from './SampleComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Fantasy Overwatch</h2>
+        </header>
+        <SampleComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
