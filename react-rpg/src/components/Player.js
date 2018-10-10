2d1b864573e90ea16b3f44118f3d5a615d7da8fd
Gary Grumbley <ggrumbley@gmail.com>
2018-10-09 20:28:00 -0700

    Add player data placeholder

--- react-rpg/src/components/Player.js
+++ react-rpg/src/components/Player.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const Player = () => <div className="player-window">Player Window</div>;
+const Player = () => (
+  <div className="player-window">
+    <dl>
+      <dt>Name:</dt>
+      <dd>Lothar</dd>
+      <dt>Class:</dt>
+      <dd>Fighter</dd>
+      <dt>Hit Points:</dt>
+      <dd>7</dd>
+      <dt>Gold:</dt>
+      <dd>20</dd>
+      <dt>XP:</dt>
+      <dd>0</dd>
+      <dt>Level:</dt>
+      <dd>1</dd>
+    </dl>
+  </div>
+);
 
 export default Player;
--- react-rpg/src/store.js
+++ react-rpg/src/store.js
@@ -1,19 +1,22 @@
 import { createStore, combineReducers } from 'redux';
 
 const initialState = {
-  xp: 0,
-  level: 1,
-  position: {
-    x: 0,
-    y: 0
+  hero: {
+    xp: 0,
+    level: 1,
+    position: {
+      x: 0,
+      y: 0
+    },
+    stats: {
+      health: 50,
+      maxHealth: 50
+    },
+    inventory: {
+      potions: 1
+    }
   },
-  stats: {
-    health: 50,
-    maxHealth: 50
-  },
-  inventory: {
-    potions: 1
-  }
+  monster: {}
 };
 
 //
