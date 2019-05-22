4ccd5a347a647f9478bbcf1ade4b546867ee3257
gary <Gary_Grumbley@intuit.com>
2019-05-22 11:08:50 -0700

    Update Webpack Config

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -1,8 +1,12 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js'],
   module: {
     rules: [
       {
@@ -16,15 +20,27 @@ module.exports = {
     extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist/'),
     publicPath: '/',
     filename: 'bundle.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new ErrorOverlayPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['You application is running here http://localhost:8080'],
+      },
+    }),
   ],
   devServer: {
     contentBase: './dist',
     hot: true,
+    port: 8080,
+    quiet: true,
   },
 };
--- redux-counter-v2/webpack.config.js
+++ redux-counter-v2/webpack.config.js
@@ -1,14 +1,18 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js'],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ['babel-loader', 'eslint-loader'],
       },
     ],
   },
@@ -16,15 +20,27 @@ module.exports = {
     extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist/'),
     publicPath: '/',
     filename: 'bundle.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
+    new ErrorOverlayPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['You application is running here http://localhost:8080'],
+      },
+    }),
   ],
   devServer: {
     contentBase: './dist',
     hot: true,
+    port: 8080,
+    quiet: true,
   },
 };
