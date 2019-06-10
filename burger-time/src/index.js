2f0124a09ecad52e6470e930cf5e04dc7722f62c
gary <Gary_Grumbley@intuit.com>
2019-06-09 21:22:46 -0700

    Add redux to BurgerBuilder

--- burger-time/src/index.js
+++ burger-time/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './index.module.css';
 import App from './components/App';
+import reducer from './reducers';
+
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer);
+
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
