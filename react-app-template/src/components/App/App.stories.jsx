157c19182a4ae5fa77569dbe303a0f2330bf63f5
Gary Grumbley <ggrumbley@gmail.com>
2020-12-14 09:45:29 -0800

    chore(react-app-template): update storybook

---
+++ react-app-template/src/components/App/App.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from '.';
+
+export default {
+  title: 'App/New Wave App',
+  component: App,
+};
+
+export const garyLogo = () => <App />;
---
+++ react-app-template/src/components/Logo/Logo.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo, { Triangle } from '.';
+
+export default {
+  title: 'Components/Logo',
+  component: Logo,
+};
+
+export const logo = () => <Logo />;
+export const triangle = () => <Triangle />;
