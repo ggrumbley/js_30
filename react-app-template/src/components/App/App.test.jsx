c85db6d5b3c3b40e62fbceb04cabb7a8edf10521
Gary Grumbley <ggrumbley@gmail.com>
2021-02-23 06:24:33 -0800

    fix: jest working with fetch

--- react-app-template/src/components/App/App.test.jsx
+++ react-app-template/src/components/App/App.test.jsx
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 import App from '.';
 import { StoreProvider } from '../../store';
 
+global.fetch = require('node-fetch');
+
 describe('App Test Suite', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
