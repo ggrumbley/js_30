a6620f340a6daf074030c6704a37add889d9073a
gary <Gary_Grumbley@intuit.com>
2021-03-02 17:39:10 -0800

    chore: add mouse interaction demo

--- dataviz-2020/src/constants/index.js
+++ dataviz-2020/src/constants/index.js
@@ -4,3 +4,6 @@ export const INITIAL_STATE = {
 
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
+
+export const CSS_NAMED_COLORS_CSV =
+  'https://gist.githubusercontent.com/ggrumbley/ea869d2e7bd1d24ee62f77f8cd68343c/raw/cssNamedColors.csv';
