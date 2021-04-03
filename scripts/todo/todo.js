9c441e252699ca4a87575efde4f424e01473ebba
gary <Gary_Grumbley@intuit.com>
2021-04-03 14:24:14 -0700

    feat(scripts): todo new todo method

--- scripts/todo/todo.js
+++ scripts/todo/todo.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
-import chalk from "chalk";
+import chalk from 'chalk';
+import rl from 'readline';
+import low from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync.js';
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ todos: [] }).write();
 
 const args = process.argv;
-const commands = ["new", "get", "complete", "help"];
+const commands = ['new', 'get', 'complete', 'help'];
 const usageText = `
 todo helps you manage you todo tasks.
 
@@ -17,6 +25,28 @@ usage:
   complete: used to mark a todo as complete
   help:     used to print the usage guide
 `;
+
+const prompt = (question) => {
+  const r = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  return new Promise((resolve, error) => {
+    r.question(question, (answer) => {
+      r.close();
+      resolve(answer);
+    });
+  });
+};
+
+const newTodo = () => {
+  const q = chalk.blue('Type in your todo\n');
+  prompt(q).then((todo) => {
+    console.log(todo);
+  });
+};
 
 const errorLog = (err) => {
   console.log(chalk.red(err));
@@ -32,11 +62,12 @@ switch (args[2]) {
     console.log(usageText);
     break;
   case 'new':
-    break
+    newTodo();
+    break;
   case 'get':
-    break
+    break;
   case 'complete':
-    break
+    break;
   default:
     errorLog('invalid command passed');
     console.log(usageText);
